2b6452b4ace80e8d22b2decde2b041150bcf26aa
Liam Mousavi <apps.khaleghi@gmail.com>
2025-01-21 23:52:32 +0330

    Load up

--- src/GraphQL/Fragments.php
+++ src/GraphQL/Fragments.php
@@ -2,5 +2,23 @@
 
 class Fragments
 {
+    public $fragments = [];
+     
+    function __construct() {
+        
+        $this->boot();
+    }
 
+    public function getFragment($fragment) {
+        return @$this->fragments[$fragment];
+    }
+
+    private function boot() {
+
+
+        foreach(glob(dirname(__FILE__).'/Fragments/*') as $fragment)   
+        {  
+            $this->fragments[explode(".",basename($fragment))[0]] = file_get_contents($fragment);
+        }
+    }
 }
--- src/GraphQL/Queries.php
+++ src/GraphQL/Queries.php
@@ -1,11 +1,49 @@
 <?php namespace AhurSystem\Wordpress\RESTGraphQL\GraphQL;
-
-require_once "Fragments.php";
+ 
 
 class Queries
 {
+    private $queries = [];
+
     function __construct() {
+        $this->boot();
+    }
+
+    public function prepare($query, $args = []) {
         
+        $data = $this->importFragments($this->queries[$query]);
+        
+        return $data;
+    }
+
+    private function importFragments($input) {
+        $fragmentsObject = new Fragments;
+
+        // Use a regex to match words starting with three dots
+        preg_match_all('/\.\.\.(\w+)/', $input, $matches);
+        $extendedFragments = $matches[1];
+        $out = $input;
+        $imported = [];
+
+        foreach ($extendedFragments as $key => $value) {
+            if(in_array($value, $imported)) continue;
+            if(!$fragmentsObject->getFragment($value)) throw new \Exception("Fragment `$value` missing, operation failed.", 1);
+
+            $frag = $this->importFragments($fragmentsObject->getFragment($value));
+            $out =  $out     . ' ' . $frag;
+            $imported[] = $value;
+            
+        }
+        return $out;
+    }
+
+
+    private function boot() {
+        
+        foreach(glob(dirname(__FILE__).'/Queries/*') as $query)   
+        {  
+            $this->queries[explode(".",basename($query))[0]] = file_get_contents($query);
+        }
     }
 
 }
