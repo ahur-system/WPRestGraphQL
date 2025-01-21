405de34d7a14f9aca94620be4978d2ed5814510e
Liam Mousavi <apps.khaleghi@gmail.com>
2025-01-21 23:52:46 +0330

    +

--- src/Base.php
+++ src/Base.php
@@ -4,6 +4,20 @@ require_once "GraphQL/Fragments.php";
 require_once "GraphQL/Queries.php";
 require_once "Rest/REST.php";
 
-class Base {
+use \AhurSystem\Wordpress\RESTGraphQL\GraphQL\Fragments;
+use \AhurSystem\Wordpress\RESTGraphQL\GraphQL\Queries;
 
+class Base {
+    private $fragments = [];
+
+    function __construct() {
+        
+    }
+    public function request($query, $args = []) {
+        
+        $data = (new Queries)->prepare($query);
+        
+        return do_graphql_request($data, $query, $args );
+    }
+    
 }
